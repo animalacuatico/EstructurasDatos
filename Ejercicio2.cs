fa8f41a74a32e5d5b6c9ee6859d7aded6030a622
aquatic_animal <angelocaceda333@gmail.com>
2025-03-24 17:50:28 +0100

    Ejercicio 2
    gg ez

--- Ejercicio2.cs
+++ Ejercicio2.cs
@@ -10,6 +10,61 @@ namespace EstructurasDatos
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Escribe estos números para añadir tareas al NPC.");
+
+            List<string> NPCActions = new List<string>();
+            bool programRunning = true;
+            while (programRunning)
+            {
+                Console.WriteLine("1: Añadir una tarea al NPC. 2: Quitar una tarea al NPC. 3: Intercambiar dos tareas del NPC. 4: Terminar el programa.");
+                Console.WriteLine("Lista de acciones: ");
+                foreach (string action in NPCActions)
+                {
+                    Console.WriteLine(action);
+                }
+                int answer = int.Parse(Console.ReadLine());
+                switch (answer)
+                {
+                    case 1:
+                    AddNPCTask(NPCActions);
+                        Console.WriteLine("Acción añadida.");
+                    break;
+                    case 2:
+                    RemoveNPCTask(NPCActions);
+                        Console.WriteLine("Acción quitada.");
+                    break;
+                    case 3:
+                    SwapNPCTask(NPCActions);
+                        Console.WriteLine("Acciones intercambiadas.");
+                    break;
+                    case 4:
+                    programRunning = false;
+                        Console.WriteLine("Programa terminado.");
+                    break;
+                }
+            }
+
+        }
+        static void AddNPCTask(List <string> NPCActions)
+        {
+            Console.WriteLine("Escribe una acción que el NPC debe cumplir.");
+            string TaskToAdd = Console.ReadLine();
+            NPCActions.Add(TaskToAdd);
+        }
+        static void RemoveNPCTask(List <string> NPCActions)
+        {
+            Console.WriteLine("Escribe el índice de la acción del NPC que quieras remover.");
+            int IndexToSubtract = int.Parse(Console.ReadLine());
+            NPCActions.RemoveAt(IndexToSubtract);
+        }
+        static void SwapNPCTask(List <string> NPCActions)
+        {
+            Console.WriteLine("Escribe dos índices para intercambiar en las acciones del NPC.");
+            int firstSwap = int.Parse(Console.ReadLine());
+            int secondSwap = int.Parse(Console.ReadLine());
+            string temp = NPCActions[firstSwap];
+            NPCActions[firstSwap] = NPCActions[secondSwap];
+            NPCActions[secondSwap] = temp;
 
         }
     }
--- obj/Debug/net8.0/EstructurasDatos.AssemblyInfo.cs
+++ obj/Debug/net8.0/EstructurasDatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstructurasDatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3727acbdab8a4f6efa912589fa64f30e2ccd848a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstructurasDatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstructurasDatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
