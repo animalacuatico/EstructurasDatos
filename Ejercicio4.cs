f012e7999dfffe15b727f978ce53504875b0134f
aquatic_animal <angelocaceda333@gmail.com>
2025-03-24 19:07:09 +0100

    Ejercicio 4 Completado
    skill issue get better

---
+++ Ejercicio4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstructurasDatos
+{
+    internal class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, char> Divisas = new Dictionary<string, char>();
+            Divisas.Add("Euro", '€');
+            Divisas.Add("Dollar", '$');
+            Divisas.Add("Yen", '¥');
+            Console.WriteLine("Escribe el nombre de una divisa para obtener su símbolo. Disponibles: Euro, Dollar, Yen");
+            string answer = Console.ReadLine();
+            if (Divisas.TryGetValue(answer, out char divisa))
+            {
+                Console.WriteLine("El símbolo de la divisa " + answer + " es " + divisa);
+            }
+            // Creo que el euro sale con ? porque la consola no conoce ese carácter.
+            else
+            {
+                Console.WriteLine("El símbolo de esa divisa no se encuentra en este diccionario.");
+            }
+        }
+
+    }
+
+}
